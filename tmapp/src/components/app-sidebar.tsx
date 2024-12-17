ec4db5a320fe14ffc4f3afa6682e3bd7ff80b256
Sai Tarun <saitarunsst@gmail.com>
2024-12-18 00:52:52 +0530

    fix: Remove unnecessary class from SidebarHeader for cleaner markup

--- tmapp/src/components/app-sidebar.tsx
+++ tmapp/src/components/app-sidebar.tsx
@@ -74,7 +74,7 @@ export function AppSidebar({ ...props }: AppSidebarProps) {
 
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader className="">
+      <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton
